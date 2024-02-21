f6358ac7d53ef1999befbab88da6adbfd60b883d
Sara Carreras <saracc621@gmail.com>
2024-02-21 20:48:08 +0100

    add sass & absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`, // Adjust the path to match your project structure
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // Set up an alias for the src directory
+    },
+  },
 });
