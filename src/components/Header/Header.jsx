201af60828d41f9d539a72e1a82054e4d1127a06
Sara Carreras <saracc621@gmail.com>
2024-02-22 19:06:04 +0100

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import photoSara from "@/assets/photo-cv-optimized.webp";
+import styles from "./Header.module.scss";
+
+export function Header() {
+  return (
+    <header>
+      <img className={styles.photo} src={photoSara} alt="Sara Carreras" />
+      <h1 className={styles.completeName}>Sara Carreras</h1>
+      <p className={styles.shortSentence}>
+        Frontend Developer you will need for (almost) any app!
+      </p>
+    </header>
+  );
+}
