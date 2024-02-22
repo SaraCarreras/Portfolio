8100f27359b383b414638b78ffea4027f9560aba
Sara Carreras <saracc621@gmail.com>
2024-02-22 19:07:08 +0100

    projects data

---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,40 @@
+import componentsImg from "../../assets/components.png";
+import stateImg from "../../assets/state-mgmt.png";
+import jsxUiImg from "../../assets/jsx-ui.png";
+import configImg from "../../assets/config.png";
+
+export const PROJECTS_DATA = [
+  {
+    src: componentsImg,
+    alt: "Corner Films",
+    title: "Corner Films",
+    description:
+      "In this app you can search for any film you'd like to know about. You can also register yourself and save your favourite films.",
+    technologies: [
+      "React",
+      "Redux",
+      "typeScript",
+      "Firebase",
+      "Sass",
+      "ReactQuery",
+    ],
+  },
+  {
+    src: stateImg,
+    alt: "Another Project",
+    title: "Another Project",
+    description: "This is the Another Project description",
+  },
+  {
+    src: stateImg,
+    alt: "Another Project",
+    title: "Another Project",
+    description: "This is the Another Project description",
+  },
+  {
+    src: stateImg,
+    alt: "Another Project",
+    title: "Another Project",
+    description: "This is the Another Project description",
+  },
+];
