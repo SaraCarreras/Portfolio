682d79bf3c075eecfdead8fdb8b035b0e9060bdf
Sara Carreras <saracc621@gmail.com>
2024-02-24 13:30:52 +0100

    added files to track them

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -10,67 +10,67 @@ import { Education } from "./MoreInfoSections/Education.jsx";
 import { ContactMe } from "./MoreInfoSections/ContactMe.jsx";
 
 export function MoreInfo() {
-  const [selectedSection, setSelectedSection] = useState("experience");
+    const [selectedSection, setSelectedSection] = useState("experience");
 
-  function handleSelect(arg) {
-    setSelectedSection(arg);
-  }
+    function handleSelect(arg) {
+        setSelectedSection(arg);
+    }
 
-  const componentMapper = {
-    experience: Experience,
-    techStack: TechStack,
-    education: Education,
-    contactMe: ContactMe,
-  };
+    const componentMapper = {
+        experience: Experience,
+        techStack: TechStack,
+        education: Education,
+        contactMe: ContactMe,
+    };
 
-  let ExamplesSection = componentMapper[selectedSection];
+    let ExamplesSection = componentMapper[selectedSection];
 
-  // if (selectedSection) {
-  //   examplesSection = (
-  //     <div id="tab-content">
-  //       {}
-  //       <h3>{MORE_INFO_DATA[selectedSection]?.title}</h3>
-  //       <span className={styles.position}>
-  //         <h5 className={styles.company}>
-  //           {MORE_INFO_DATA[selectedSection]?.company}
-  //         </h5>
-  //         <p className={styles.date}>{MORE_INFO_DATA[selectedSection]?.date}</p>
-  //       </span>
+    // if (selectedSection) {
+    //   examplesSection = (
+    //     <div id="tab-content">
+    //       {}
+    //       <h3>{MORE_INFO_DATA[selectedSection]?.title}</h3>
+    //       <span className={styles.position}>
+    //         <h5 className={styles.company}>
+    //           {MORE_INFO_DATA[selectedSection]?.company}
+    //         </h5>
+    //         <p className={styles.date}>{MORE_INFO_DATA[selectedSection]?.date}</p>
+    //       </span>
 
-  //       <p>
-  //         {MORE_INFO_DATA[selectedSection]?.content.map((el) =>
-  //           console.log(el)
-  //         )}
-  //       </p>
-  //       <pre>
-  //         <code>{MORE_INFO_DATA[selectedSection]?.code}</code>
-  //       </pre>
-  //     </div>
-  //   );
-  // }
-  return (
-    <Section id="examples" title="More Info">
-      <Tab
-        ButtonsContainer="menu"
-        buttons={
-          <>
-            {Object.keys(MORE_INFO_DATA).map((el, index) => (
-              <TabButton
-                key={index}
-                onClick={() => handleSelect(el)}
-                isSelected={selectedSection === el}
-              >
-                {MORE_INFO_DATA[el].title}
-              </TabButton>
-            ))}
-          </>
-        }
-      >
-        <div id="tab-content">
-          {" "}
-          {<ExamplesSection data={MORE_INFO_DATA[selectedSection]} />}
-        </div>
-      </Tab>
-    </Section>
-  );
+    //       <p>
+    //         {MORE_INFO_DATA[selectedSection]?.content.map((el) =>
+    //           console.log(el)
+    //         )}
+    //       </p>
+    //       <pre>
+    //         <code>{MORE_INFO_DATA[selectedSection]?.code}</code>
+    //       </pre>
+    //     </div>
+    //   );
+    // }
+    return (
+        <Section id="examples" title="More Info">
+            <Tab
+                ButtonsContainer="menu"
+                buttons={
+                    <>
+                        {Object.keys(MORE_INFO_DATA).map((el, index) => (
+                            <TabButton
+                                key={index}
+                                onClick={() => handleSelect(el)}
+                                isSelected={selectedSection === el}
+                            >
+                                {MORE_INFO_DATA[el].title}
+                            </TabButton>
+                        ))}
+                    </>
+                }
+            >
+                <div id="tab-content">
+                    {" "}
+                    {<ExamplesSection data={MORE_INFO_DATA[selectedSection]} />}
+                </div>
+            </Tab>
+        </Section>
+    );
 }
---
+++ src/components/MoreInfo/utils.js
@@ -0,0 +1,11 @@
+import { Experience } from "./MoreInfoSections/Experience.jsx";
+import { TechStack } from "./MoreInfoSections/TechStack.jsx";
+import { Education } from "./MoreInfoSections/Education.jsx";
+import { ContactMe } from "./MoreInfoSections/ContactMe.jsx";
+
+export const componentMapper = {
+  experience: Experience,
+  techStack: TechStack,
+  education: Education,
+  contactMe: ContactMe,
+};
