4aa1aa7a6e282c9c19f6e3e6682c0762b84cda6f
Sara Carreras <saracc621@gmail.com>
2024-02-24 13:29:48 +0100

    updated to TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
-
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.scss";
 
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(entryPoint).render(<App />);
