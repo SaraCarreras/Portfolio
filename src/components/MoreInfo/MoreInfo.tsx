d3c921eccbcff99ff09cac783153f6ee7feeefe9
Sara Carreras <saracc621@gmail.com>
2024-02-25 20:21:01 +0100

    types created

--- src/components/MoreInfo/MoreInfo.tsx
+++ src/components/MoreInfo/MoreInfo.tsx
@@ -8,15 +8,33 @@ import { Experience } from "./MoreInfoSections/Experience";
 import { TechStack } from "./MoreInfoSections/TechStack";
 import { Education } from "./MoreInfoSections/Education";
 import { ContactMe } from "./MoreInfoSections/ContactMe";
+import { SelectedComponentType } from "./types/MoreInfoComponentType";
+import {
+    iContactMe,
+    iEducation,
+    iExperience,
+    iTechStack,
+} from "./interfaces/iMoreInfoData";
+import { iMoreInfoData } from "./interfaces/iMoreInfoData";
 
 export function MoreInfo() {
-    const [selectedSection, setSelectedSection] = useState("experience");
+    const [selectedSection, setSelectedSection] =
+        useState<SectionKey>("experience");
 
-    function handleSelect(arg) {
+    function handleSelect(arg: SectionKey) {
         setSelectedSection(arg);
     }
 
-    const componentMapper = {
+    type SectionKey = keyof ComponentMapper;
+
+    type ComponentMapper = {
+        experience: React.ComponentType<{ data: iExperience }>;
+        techStack: React.ComponentType<{ data: iTechStack }>;
+        education: React.ComponentType<{ data: iEducation }>;
+        contactMe: React.ComponentType<{ data: iContactMe }>;
+    };
+
+    const componentMapper: ComponentMapper = {
         experience: Experience,
         techStack: TechStack,
         education: Education,
@@ -25,6 +43,9 @@ export function MoreInfo() {
 
     let ExamplesSection = componentMapper[selectedSection];
 
+    
+
+    console.log(MORE_INFO_DATA[selectedSection]);
     return (
         <Section id="examples" title="More Info">
             <Tab
@@ -34,18 +55,26 @@ export function MoreInfo() {
                         {Object.keys(MORE_INFO_DATA).map((el, index) => (
                             <TabButton
                                 key={index}
-                                onClick={() => handleSelect(el)}
+                                onClick={() => handleSelect(el as SectionKey)}
                                 isSelected={selectedSection === el}
                             >
-                                {MORE_INFO_DATA[el].title}
+                                {(MORE_INFO_DATA[el as SectionKey].title, el)}
                             </TabButton>
                         ))}
                     </>
                 }
             >
                 <div id="tab-content">
-                    {" "}
-                    {<ExamplesSection data={MORE_INFO_DATA[selectedSection]} />}
+                    {
+                        <ExamplesSection
+                            data={
+                                MORE_INFO_DATA[selectedSection] as iExperience &
+                                    iTechStack &
+                                    iEducation &
+                                    iContactMe
+                            }
+                        />
+                    }
                 </div>
             </Tab>
         </Section>
