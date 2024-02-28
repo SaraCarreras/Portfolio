0339b2298368bc7a1329d7c3762fd53e4c871db6
Sara Carreras <saracc621@gmail.com>
2024-02-28 16:29:32 +0100

    navbar & main

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+export function NavBar() {
+  return (
+    <nav>
+      <ul className="flex flex-row gap-x-10">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header";
+import { NavBar } from "../components/NavBar";
+import "@/index.css";
+import "@fontsource/open-sans";
+
+function App() {
+  return (
+    <div className="">
+      <NavBar />
+      <Header />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
