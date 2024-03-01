ee71a1c06c2a5858e84c229c28f399f6c3a37281
Sara Carreras <saracc621@gmail.com>
2024-03-01 12:30:37 +0100

    added svgs to component support Darkmode

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,6 +1,5 @@
 declare module "*.module.css";
 declare module "*.module.scss";
 declare module "*.webp";
-declare module "*.jpg";
-declare module "*.png";
 declare module "*.svg";
+declare module "*.tsx";
