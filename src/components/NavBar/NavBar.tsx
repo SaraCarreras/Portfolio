bb5aec6619ff21c2b7d6a9d4e342b9cd54a59dd2
Sara Carreras <saracc621@gmail.com>
2024-03-07 12:49:21 +0100

    added navbar buttons

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,41 @@
+import { SvgDarkMode } from "@/icons/SvgDarkMode";
+import { SvgLightMode } from "@/icons/SvgLightMode";
+import { useState } from "react";
+import { SvgKey } from "./types";
+
+export function NavBar() {
+  const [themeMode, setThemeMode] = useState<SvgKey>("dark");
+
+  const SvgModeMapper = {
+    dark: <SvgDarkMode />,
+    light: <SvgLightMode />,
+  };
+
+  const SvgComponente = SvgModeMapper[themeMode];
+
+  function changeMode() {
+    themeMode === "dark" ? setThemeMode("light") : setThemeMode("dark");
+  }
+
+  return (
+    <nav className="flex justify-center items-center w-[1120px] py-5 mx-auto">
+      <ul className="flex flex-row gap-x-10 self-end">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <button onClick={changeMode}>{SvgComponente}</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/NavBar/types.ts
@@ -0,0 +1,6 @@
+export type SvgKey = keyof SvgModeMapper;
+
+export type SvgModeMapper = {
+  dark: JSX.Element;
+  light: JSX.Element;
+};
