07b7f5cf6218c1a0a144edcec6277a5b67244b1a
Sara Carreras <saracc621@gmail.com>
2024-03-07 21:03:54 +0100

    updated footer svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
-import { Up } from "@/icons/Up";
+import { SvgArrowUp } from "@/icons/SvgArrowUp";
 
 export function Footer() {
   return (
     <div className="flex justify-between p-8 max-w-[100vw] ">
       <p>Sara Carreras ✨</p>
       <button className="rounded-full border border-solid border-white/25 bg-white/5 p-2 hover:scale-110 hover:bg-white/10 transition">
-        <a href="#header" className="flex p-1">
-          <Up />
+        <a href="#header">
+          <SvgArrowUp />
         </a>
       </button>
     </div>
