dd26b88e01afa16b166411559240013999360ad2
Sara Carreras <saracc621@gmail.com>
2024-03-07 19:37:59 +0100

    experience styles with icon

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,12 +1,16 @@
 import { EXPERIENCE_DATA } from "@/constants/Experience";
 import { ExperienceItem } from "./ExperienceItem";
-import { SectionContainer } from "./SectionContiainer";
+import { SectionContainer } from "./SectionContainer";
+import { SvgExperience } from "@/icons/SvgExperience";
 
 export function Experience() {
   return (
     <SectionContainer id="experience">
-      <h2 className="text-2xl bold pb-8">Experience</h2>
-      <ol className="relative border-s border-purple-200 dark:border-purple-700">
+      <h2 className="text-2xl bold pb-8 flex gap-x-2 items-center">
+        <SvgExperience />
+        Experience
+      </h2>
+      <ol className="relative border-s border-purple-200 dark:border-purple-700 ml-4">
         {EXPERIENCE_DATA.map((item, index) => {
           const isLatestExp = index === 0;
           return (
