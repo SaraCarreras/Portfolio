520a2c52101f5d181313affc5ed4bfb5fad98009
Sara Carreras <saracc621@gmail.com>
2024-03-07 12:48:40 +0100

    update config for absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path, { resolve } from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
 });
