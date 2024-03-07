7a8337275c3295ba3dd0ed703bf37fd145bd025c
Sara Carreras <saracc621@gmail.com>
2024-03-07 20:26:47 +0100

    updated svg arrow styles

--- src/icons/SvgArrowUp.tsx
+++ src/icons/SvgArrowUp.tsx
@@ -2,17 +2,33 @@ export function SvgArrowUp() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="feather feather-arrow-up"
+    >
+      <line x1="12" y1="19" x2="12" y2="5"></line>
+      <polyline points="5 12 12 5 19 12"></polyline>
+    </svg>
+  );
+}
+
+/* <svg
+      xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="2.3"
+      stroke-width="2"
       stroke="currentColor"
-      className="w-6 h-5"
+      className="w-6 h-6"
     >
       <path
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
       />
-    </svg>
-  );
-}
+    </svg> */
