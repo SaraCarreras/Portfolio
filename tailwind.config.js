0b6fe1b4a60c142ef38c140799b9f44adc6a563b
Sara Carreras <saracc621@gmail.com>
2024-03-13 16:20:27 +0100

    preloading font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        OpenSansRegular: ["OpenSansRegular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
