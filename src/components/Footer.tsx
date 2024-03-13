268843b13870f1d31496fa838e3a553993a85f88
Sara Carreras <saracc621@gmail.com>
2024-03-13 16:22:07 +0100

    added dark mode styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { SvgArrowUp } from "@/icons/SvgArrowUp";
 
 export function Footer() {
   return (
-    <div className="flex justify-between p-8 max-w-[100vw] ">
+    <div className="flex justify-between p-8 max-w-[100vw] dark:text-gray-300">
       <p>Sara Carreras ✨</p>
       <button className="rounded-full border border-solid border-white/25 bg-white/5 p-2 hover:scale-110 hover:bg-white/10 transition">
         <a href="#header">
