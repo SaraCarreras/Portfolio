4a4f5a8aff8608b492428800285426f55d8e95b7
Sara Carreras <saracc621@gmail.com>
2024-03-14 13:32:59 +0100

    added col style

--- src/components/ProjectsSection/ProjectItem.tsx
+++ src/components/ProjectsSection/ProjectItem.tsx
@@ -11,40 +11,46 @@ export function ProjectItem({
   technology,
 }: iProjectItem) {
   return (
-    <article>
-      <h3 className="text-2xl font-semibold  text-purple-500 mt-6 mb-4 ">
-        {title}
-      </h3>
-      <p className="text-lg text-pretty">{description}</p>
+    <article className="flex flex-col space-x-0 space-y-8 md:flex-row md:space-x-8 md:space-y-0">
+      <div className="flex items-center mx-auto w-full  md:w-1/2 md:items-start">
+        <div className="relative flex flex-col items-center col-span-6 row-span-5 gap-8 transition duration-500 ease-in-out transform shadow-xl overflow-clip rounded-xl sm:rounded-xl md:hover:-translate-y-1 md:hover:shadow-2xl lg:border lg:border-gray-800 lg:hover:border-gray-700 lg:hover:bg-gray-800/50">
+          <img
+            alt="project's screenshot"
+            className="object-cover object-top max-w-xl w-full transition duration-500 sm:h-full md:scale-110 md:hover:scale-105"
+            loading="lazy"
+            src={image}
+          />
+        </div>
+      </div>
 
-      <ul className="flex gap-4 flew-row flex-wrap my-4 ">
-        {technology?.map((el) => {
-          return (
-            <li key={el.name}>
-              <span
-                className={`flex gap-x-2 rounded-full items-center text-md   ${el.class} py-1 px-4 `}
-              >
-                <el.icon />
+      <div className="w-full md:w-1/2 md:max-w-lg">
+        <h3 className="text-2xl font-semibold  text-purple-500  ">{title}</h3>
+        <ul className="flex gap-4 flew-row flex-wrap my-4 ">
+          {technology?.map((el) => {
+            return (
+              <li key={el.name}>
+                <span
+                  className={`flex gap-x-2 rounded-full items-center text-md   ${el.class} py-1 px-4 `}
+                >
+                  <el.icon />
 
-                {el.name}
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-      <img
-        className="rounded shadow-2xl shadow-gray dark:shadow/white"
-        src={image}
-        alt="project's screenshot"
-      />
-      <span className="flex gap-3 mt-4 ">
-        <LinkButton href={preview}>
-          <SvgPreview /> View
-        </LinkButton>
-        <LinkButton href={github}>
-          <SvgGithub /> Github
-        </LinkButton>
-      </span>
+                  {el.name}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+        <p className="text-lg text-pretty">{description}</p>
+
+        <span className="flex gap-3 mt-4 ">
+          <LinkButton href={preview}>
+            <SvgPreview /> View
+          </LinkButton>
+          <LinkButton href={github}>
+            <SvgGithub /> Github
+          </LinkButton>
+        </span>
+      </div>
     </article>
   );
 }
--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -5,21 +5,23 @@ import { PROJECTS_DATA } from "@/components/ProjectsSection/ProjectsData";
 
 const ProjectsSection = () => {
   return (
-    <SectionContainer id="projects">
-      <h2 className="text-3xl font-semibold flex gap-x-2 items-center">
-        <SvgCode />
-        Projects
-      </h2>
-      <ol className="relative   ">
-        {PROJECTS_DATA.map((project) => {
-          return (
-            <li key={project.preview}>
-              <ProjectItem {...project} />
-            </li>
-          );
-        })}
-      </ol>
-    </SectionContainer>
+    <div className="flex flex-col gap-y-16 ">
+      <SectionContainer id="projects">
+        <h2 className="text-3xl font-semibold flex gap-x-2 items-center pb-8">
+          <SvgCode />
+          Projects
+        </h2>
+        <ol className="relative   ">
+          {PROJECTS_DATA.map((project) => {
+            return (
+              <li key={project.preview}>
+                <ProjectItem {...project} />
+              </li>
+            );
+          })}
+        </ol>
+      </SectionContainer>
+    </div>
   );
 };
 export default ProjectsSection;
