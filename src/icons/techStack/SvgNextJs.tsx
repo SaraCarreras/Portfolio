4ea75c1534e668056489b81dc12a375dfa69756f
Sara Carreras <saracc621@gmail.com>
2024-03-17 09:56:13 +0100

    update style props

--- src/icons/techStack/SvgNextJs.tsx
+++ src/icons/techStack/SvgNextJs.tsx
@@ -25,7 +25,7 @@ export function SvgNextJs() {
           r="87"
           fill="black"
           stroke="white"
-          stroke-width="6"
+          strokeWidth="6"
         />
         <path
           d="M149.508 157.52L69.142 54H54V125.97H66.1136V69.3836L139.999 164.845C143.333 162.614 146.509 160.165 149.508 157.52Z"
@@ -48,8 +48,8 @@ export function SvgNextJs() {
           y2="160.5"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="white" stop-opacity="0" />
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_408_139"
@@ -59,8 +59,8 @@ export function SvgNextJs() {
           y2="106.875"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="white" stop-opacity="0" />
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
         </linearGradient>
       </defs>
     </svg>
