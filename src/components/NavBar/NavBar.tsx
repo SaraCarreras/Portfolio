32990da0ae325516cf03a4537d17d0252c0140fe
Sara Carreras <saracc621@gmail.com>
2024-03-19 11:11:11 +0100

    fixed padding gap styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@ import { SvgLightMode } from "@/icons/SvgLightMode";
 import { useEffect, useState } from "react";
 import { SvgKey } from "./types";
 import { returnOppositeTheme } from "@/utils/returnOppositeTheme";
+import { navItems } from "./NavBarData";
 
 export function NavBar() {
   const [themeMode, setThemeMode] = useState<SvgKey>(() => {
@@ -31,37 +32,75 @@ export function NavBar() {
     }
   }, [themeMode]);
 
-  //sm:w-[740px]
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const navItems = document.querySelectorAll("header nav a");
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      console.log(entries, "entries");
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        console.log(entry, "entry");
+        if (entry.isIntersecting) {
+          navItems.forEach((item) => {
+            if (item.getAttribute("aria-label") === entry.target.id) {
+              item.classList.add("text-yellow-500");
+            } else {
+              item.classList.remove("text-yellow-500");
+            }
+          });
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    });
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    // Cleanup function
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "hidden") {
+        observer.disconnect();
+      } else {
+        sections.forEach((section) => {
+          observer.observe(section);
+        });
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <nav className="flex mx-auto sticky dark:bg-white/40 w-fit top-0 z-10 border rounded-full border-black bg-purple-400/10 backdrop-blur ">
-      <ul className="flex flex-row items-center self-end font-bold sm:text-lg sm:gap-x-5 ">
-        <li className="hover:scale-110">
-          <a
-            className=" dark:hover:text-purple-900 hover:text-purple-800 pl-5"
-            href="#header"
-          >
-            Header
-          </a>
-        </li>
-        <li className="hover:scale-110">
-          <a className=" hover:text-purple-900 px-2" href="#experience">
-            Experience
-          </a>
-        </li>
-
-        <li className="hover:scale-110">
-          <a className=" hover:text-purple-900 px-2" href="#projects">
-            Projects
-          </a>
-        </li>
-        <li className="hover:scale-110">
-          <a className=" hover:text-purple-900 px-2" href="#about">
-            About
-          </a>
-        </li>
+      <ul className="flex flex-row items-center self-end font-bold sm:text-lg gap-x-3 px-3  sm:gap-x-5 sm:px-5">
+        {navItems.map((item) => {
+          return (
+            <li className="hover:scale-110">
+              <a
+                className="dark:hover:text-purple-900 hover:text-purple-800"
+                href={item.url}
+                aria-label={item.label}
+              >
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
         <li>
+          {/* px-3 */}
           <button
-            className="mt-2  hover:text-purple-900 pr-4  hover:scale-110"
+            className="mt-2  hover:text-purple-900 hover:scale-110"
             onClick={handleChangeTheme}
           >
             {SvgComponente}
