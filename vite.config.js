c9f440ab1396759402b749c5fff689de8171638a
Hagaren5 <oriolsm93@gmail.com>
2024-05-19 18:48:38 +0200

    changed to vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variables.scss";`, 
-      },
+    plugins: [react(), tsconfigPaths],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/styles/variables.scss";`,
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"), 
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-  },
 });
